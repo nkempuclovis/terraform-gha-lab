3ed0ed9c019bd14244d4c4de5214d6a352682db4
Clovis Foji <nkempu20@yahoo.com>
2023-10-23 22:58:05 -0400

    Added new stuff

--- main.tf
+++ main.tf
@@ -58,7 +58,7 @@ resource "aws_instance" "web" {
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
   ingress {
-    from_port   = 8080
+    from_port   = 8081
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
